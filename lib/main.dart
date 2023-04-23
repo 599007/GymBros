e47fd9ab7113e9a8e77a3268544cc7a95972148b
Espen Grebstad <599007@stud.hvl.no>
2023-04-24 00:19:15 +0200

    Made loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertest/Profile.dart';
+import 'package:fluttertest/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,61 +29,70 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('hei');
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
   Color bgColor = Colors.white10;
+  final _formKey = GlobalKey<FormState>();
+
+  String? _email ='';
+  String? _password ='';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("tittel"),
+        title: Text("Login"),
       ),
-      body: Center(
-        child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Card(
-                margin: const EdgeInsets.all(10),
-                color: bgColor,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.network(
-                        'https://images.squarespace-cdn.com/content/v1/57d29fb7414fb53b53085151/1496230493344-MFBY8TUF80G2JZUDJSS4/papeg%C3%B8ye-dyrepark',
-                        width: 200),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SecondPage()));
-                        },
-                        child: Text('Redirect')),
-                    TextButton(
-                        onPressed: () {
-                          setState(() {
-                            if (bgColor == Colors.white10) {
-                              bgColor = Colors.green;
-                            } else {
-                              bgColor = Colors.white10;
-                            }
-                          });
-                        },
-                        child: Text('Endre farge')),
-                  ],
-                ),
-              );
-            }),
-      ),
+      body: Padding(padding: EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email'),
+              validator: (value){
+                if(value?.isEmpty ?? true){
+                  return 'Please enter your email';
+                }
+                return null;
+              },
+              onSaved: (value){
+                _email = value;
+              },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: false,
+                validator: (value){
+                  if(value?.isEmpty ?? true){
+                    return 'Please enter you password';
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  _password = value;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: (){
+                  final form = _formKey.currentState;
+                  if(form != null && form.validate()){
+                  form.save();
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => profilePage()));
+                  }
+                },
+                child: Text('Login'),
+              ),
+              TextButton(
+                onPressed: (){
+                Navigator.push(context,  MaterialPageRoute(
+                  builder: (context) => registerPage()));
+              }, child: Text('Register new gymbro'),
+              )
+          ]
+        )))
     );
   }
 }
