57e52780470a135b42b8af774144420bb06cf2ff
599007 <599007@stud.hvl.no>
2023-04-25 20:44:16 +0200

    Did stuff

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,14 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class profilePage extends StatelessWidget {
+class profilePage extends StatefulWidget {
   const profilePage({super.key});
 
   @override
+  State<profilePage> createState() => _profilePageState();
+}
+
+class _profilePageState extends State<profilePage> {
+  void name(params) {
+    
+  }
+
+  String url = "";
+
+  @override
   Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text('${_auth.currentUser?.email ?? ''}'),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection("users").doc(_auth.currentUser?.uid).set({
+                  "weight": 100,
+                  "email": _auth.currentUser?.email,
+                  "height": 188
+                });
+              },
+              child: Text('Espen')
+              ),
+              TextButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection("users").doc(_auth.currentUser!.uid).get().then((value) {
+                  setState(() {
+                    url = value.data()!["url"];
+                  });
+                });
+              },
+              child: Text('get info')
+              ),
+              Image.network(url)
+          ],
+        )
+        )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertest/Profile.dart';
 import 'package:fluttertest/register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -30,89 +34,113 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Color bgColor = const Color.fromARGB(255, 27, 27, 27);
+  Color bgColor = Color.fromARGB(255, 255, 255, 255);
   final _formKey = GlobalKey<FormState>();
 
-  String? _email ='';
-  String? _password ='';
+  String _email = '';
+  String _password = '';
+
+  signIn() async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: _email, password: _password);
+
+      if (credential.user != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => profilePage()));
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
-      body: SingleChildScrollView(
-      child: Padding(padding: EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-              color: Color.fromARGB(110, 124, 120, 120),
-              borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'Email', border: InputBorder.none),
-                validator: (value){
-                  if(value?.isEmpty ?? true){
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                onSaved: (value){
-                  _email = value;
-                },
-                ),
-            ),
-            SizedBox(height: 5.0),
-              Container(
-                decoration: BoxDecoration(
-                color: Color.fromARGB(110, 124, 120, 120),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-                child: TextFormField(
-                  decoration: InputDecoration(labelText: 'Password', border: InputBorder.none),
-                  obscureText: false,
-                  validator: (value){
-                    if(value?.isEmpty ?? true){
-                      return 'Please enter you password';
-                    }
-                    return null;
-                  },
-                  onSaved: (value){
-                    _password = value;
-                  },
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              ElevatedButton(
-                onPressed: (){
-                  final form = _formKey.currentState;
-                  if(form != null && form.validate()){
-                  form.save();
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => profilePage()));
-                  }
-                },
-                child: Text('Login'),
-              ),
-              const SizedBox(height: 50.0),
-              Align(
-              alignment: Alignment.bottomCenter,
-              child: TextButton(
-                onPressed: (){
-                Navigator.push(context,  MaterialPageRoute(
-                  builder: (context) => registerPage()));
-              }, child: Text('Register new gymbro'),
-              )
-              )
-        ]
-              )
-      ))
-      
-        )
-        );    
+        resizeToAvoidBottomInset: false,
+        backgroundColor: bgColor,
+        body: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+                key: _formKey,
+                child: Column(children: <Widget>[
+                  const SizedBox(height: 80),
+                  ClipRRect(
+                      child: Image.asset("assets/logo.png",
+                          height: 180, width: 180),
+                      borderRadius: BorderRadius.circular(50)),
+                  const SizedBox(height: 70),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Color.fromARGB(192, 222, 222, 222),
+                          borderRadius: BorderRadius.circular(0.0),
+                          
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: InputBorder.none),
+                          validator: (value) {
+                            return value?.isEmpty ?? true
+                                ? 'Please enter your email'
+                                : null;
+                          },
+                          onSaved: (value) {
+                            _email = value ?? "";
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 5.0),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Color.fromARGB(192, 222, 222, 222),
+                          borderRadius: BorderRadius.circular(0.0),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Password', border: InputBorder.none),
+                          obscureText: false,
+                          validator: (value) {
+                            return value?.isEmpty ?? true
+                                ? 'Please enter you password'
+                                : null;
+                          },
+                          onSaved: (value) {
+                            _password = value ?? "";
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final form = _formKey.currentState;
+                          if (form != null && form.validate()) {
+                            form.save();
+                            signIn();
+                          }
+                        },
+                        child: Text('Login'),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => registerPage()));
+                    },
+                    child: Text('Register new gymbro'),
+                  )
+                ]))));
   }
 }
